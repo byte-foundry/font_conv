a0b1ed5ece273ec707a2aa9b4583026f1e418611
Bill <bill.rousseau@student.42.fr>
2014-07-16 14:49:25 +0200

    bash conversion script

---
+++ convert.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env fontforge
+
+i=1
+format=".ttf"
+while ( i<$argc )
+  if ( $argv[i]=="-format" || $argv[i]=="--format" )
+    i=i+1
+    if ( i<$argc )
+      format = $argv[i]
+#      if ( format!=".ttf" && format!=".otf" && \
+#	  format!=".pfb" && format!=".svg" )
+#	Error( "Expected one of '.ttf', '.otf', '.pfb' or '.svg' for format" )
+#      endif
+    endif
+  else
+    Open($argv[i])
+    Generate($argv[i]:r + format)
+  endif
+  i = i+1
+endloop
