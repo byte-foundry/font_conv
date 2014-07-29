a0b1c62f87066cbb426db9c044cbe07080f5432b
Bill <bill.rousseau@student.42.fr>
2014-07-29 16:20:05 +0200

    bash scripts w/o sudo npms

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo prototypo | sudo -S apt-get update
 sudo apt-get -y install npm
 sudo npm install pm2@latest -g
 sudo apt-get -y install fontforge
-sudo npm install formidable
+npm install formidable
 sudo apt-get -y install nodejs
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo pm2 start server.js
--- uninstall.sh
+++ uninstall.sh
@@ -7,6 +7,6 @@ sudo pm2 kill
 sudo npm uninstall pm2 && sudo rm /usr/local/bin/pm2
 sudo rm /usr/bin/node && sudo rm /usr/bin/nodejs
 sudo apt-get -y uninstall nodejs || sudo apt-get -y remove nodejs
-sudo npm uninstall formidable && sudo rm -rf node_modules
+npm uninstall formidable && sudo rm -rf node_modules
 sudo apt-get -y uninstall fontforge || sudo apt-get -y remove fontforge
 sudo apt-get -y uninstall npm || sudo apt-get -y remove npm
