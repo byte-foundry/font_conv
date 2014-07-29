20abd3b9a31d1929bcd18b9297946eb186f4853a
Bill <bill.rousseau@student.42.fr>
2014-07-29 16:10:54 +0200

    bash scripts : for shared folder in home only

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 echo prototypo | sudo -S apt-get update
 sudo apt-get -y install npm
+sudo npm install pm2@latest -g
 sudo apt-get -y install fontforge
 sudo npm install formidable
 sudo apt-get -y install nodejs
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo pm2 start server.js
