2056a8dc2b08c8f81fc92ea3f39b3527ac5c1f3b
Bill <bill.rousseau@student.42.fr>
2014-08-04 16:33:38 +0200

    uninstall script updated with Q and adm-zip

--- uninstall.sh
+++ uninstall.sh
@@ -5,8 +5,10 @@ sudo pm2 stop server
 sudo pm2 delete server
 sudo pm2 kill
 sudo npm uninstall pm2 && sudo rm /usr/local/bin/pm2
-sudo rm /usr/bin/node
-sudo apt-get -y uninstall nodejs || sudo apt-get -y remove nodejs
 sudo npm uninstall formidable && sudo rm -rf node_modules
-sudo apt-get -y uninstall fontforge || sudo apt-get -y remove fontforge
-sudo apt-get -y uninstall npm || sudo apt-get -y remove npm
+sudo npm uninstall adm-zip
+sudo npm uninstall q
+sudo rm /usr/bin/node
+sudo apt-get -y remove nodejs
+sudo apt-get -y remove fontforge
+sudo apt-get -y remove npm
