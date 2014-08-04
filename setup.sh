feff09869330fac541709e7df81a1113f028d37f
Ubuntu <bill@fontconvertor.fontconvertor.a5.internal.cloudapp.net>
2014-08-04 14:25:19 +0000

    update setup.sh file with adm-zip and Q, new server

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 apt-get update
 apt-get -y install npm
 npm install pm2@latest -g
-apt-get -y install fontforge
 npm install formidable
+npm install adm-zip
+npm install q
+apt-get -y install fontforge
 apt-get -y install nodejs
 ln -s /usr/bin/nodejs /usr/bin/node
 pm2 start server.js
